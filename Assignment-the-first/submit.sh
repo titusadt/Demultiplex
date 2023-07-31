609fe036dab8956979773b6bdb71370bbdc04d8b
Temi Titus Adewunmi <temi@login3.talapas.uoregon.edu>
2023-07-31 14:30:00 -0700

    script to submit code and generate plot

---
+++ Assignment-the-first/submit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=compute
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=32G
+
+conda activate bgmp_py311
+
+file_R1='/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz'
+file_R2='/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz'
+file_R3='/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz'
+file_R4='/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz'
+
+/usr/bin/time -v ./distribution.py -f $file_R1 -l 101 -p read1_plot.png
+/usr/bin/time -v ./distribution.py -f $file_R2 -l 8 -p read2_plot.png
+/usr/bin/time -v ./distribution.py -f $file_R3 -l 8 -p read3_plot.png
+/usr/bin/time -v ./distribution.py -f $file_R4 -l 101 -p read4_plot.png
